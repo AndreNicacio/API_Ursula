a126ba691cb350d7d0991adea8d57661c4e48ef2
Andre Ferreira Nicacio <43629300+AndreNicacio@users.noreply.github.com>
2022-02-14 13:07:38 -0300

    primeiro commit

---
+++ routes/score.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/', (req, res, next) => {
+
+
+    res.status(200).send({
+        mensagem: 'Ursula will rule the world'
+    })
+})
+
+router.post('/', (req, res, next) => {
+
+
+    const score = {
+        fatorDeRisco: req.body.fator,
+        doencasAgrupadas: req.body.doencas
+    };
+
+    res.status(201).send({
+        mensagem: 'Usando o post no score deu certo',
+        score: score
+    })
+})
+
+module.exports = router;
